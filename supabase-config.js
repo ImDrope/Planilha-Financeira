37da91873e02f27f13339b88cf881356b86836eb
ImDrope <gamerbr5587@gmail.com>
2026-07-22 17:05:46 -0300

    Usar domínio oficial nos retornos de autenticação

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.DESPESA_MENSAL_CONFIG = Object.freeze({
   supabaseUrl: "https://uumczltusdkpjtujphki.supabase.co",
   supabasePublishableKey: "sb_publishable_PNuraJN6b0i1MywFZ4P0cw_ogChn_B4",
-  authRedirectUrl: "https://imdrope.github.io/Planilha-Financeira/"
+  authRedirectUrl: "https://app.despesamensal.com.br/"
 });
 
